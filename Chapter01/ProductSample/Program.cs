27ce02c6ac8bf88e00eb1d71459ea2549d56eedd
infosys <infosys@C4229>
2025-04-21 09:57:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+
+
+            //税抜きの価格を表示
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "です。");
+            //消費税額の表示
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "です。"); 
+            //税込み価格の表示
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax()  + "です。");
+        }
+    }
+}
