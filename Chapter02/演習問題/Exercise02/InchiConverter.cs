f3b93e0b082b5a8b1210dca897aa59f28019cc5c
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-04-22 11:44:20 +0900

    インチからメートル

---
+++ Chapter02/演習問題/Exercise02/InchiConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    public static class InchiConverter {
+
+        //定数
+        private const double ratio = 0.0254;
+
+        //インチからメートルを求める
+        public static double ToMeter(double inchi) {
+            return inchi * ratio;
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            PrintInchiToMeterList(1, 10);
+        }
+
+        // インチからメートルへの対応表を出力
+        static void PrintInchiToMeterList(int start, int end) {
+            for (int inchi = start; inchi <= end; inchi++) {
+                double meter = InchiConverter.ToMeter(inchi);
+                Console.WriteLine($"{inchi}ft = {meter:0.0000}m");
+            }
+        }
+
+    }
+}
