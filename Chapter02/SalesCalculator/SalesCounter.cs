c4848babd5c9b263b37690c86ec54d21ef69f835
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-05-09 15:20:46 +0900

    コンストラクタの引数変更

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -10,8 +10,8 @@ namespace SalesCalculator {
         private readonly List<Sale> _sales;
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filepath) {
+            _sales = ReadSales(filepath);
         }
 
         //店舗別売り上げを求める
