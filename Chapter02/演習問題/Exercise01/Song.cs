aebd167e805e4393a1776332d6e11c389a9a882c
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-05-13 09:34:48 +0900

    2.1.1と2.1.2が完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    //2.1.1
+    public class Song
+    {
+
+    }
+    //2.1.2
+
+
+
+}
