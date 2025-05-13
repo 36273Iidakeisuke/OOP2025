55a3c162e04ac471f9abc0809e8ea856a8f37610
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-05-13 10:27:22 +0900

    ２．１．３   完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,12 +1,14 @@
 ﻿namespace Exercise01 {
     public class Program {
         static void Main(string[] args) {
-         //2.1.3
-
-
-
-
-
+            //2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
         }
 
         //2.1.4
