f54caf8cfda8e531c351f52ae97ccf816087d338
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-05-27 11:09:53 +0900

    7-1完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            numbers[^2..^0].ToList().ForEach(Console.WriteLine);
+        }
+       
+
+        private static void Exercise3(int[] numbers) {
+            var index2 = numbers.Select(s => s.ToString("000"));
+            foreach(var s in index2) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+            var index2 = numbers.Order().ToList();
+            index2[0..3].ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise5(int[] numbers) {
+            Console.WriteLine(numbers.Distinct().Count(s => s >10));
+        }
+    }
+}
---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            var numbers = Enumerable.Range(1, 100);
+
+            ////合計値を出力
+            //Console.WriteLine(numbers.Sum());
+            ////平均値を出力
+            //Console.WriteLine(numbers.Average());
+            ////偶数の合計値
+            //Console.WriteLine(numbers.Where(s => s % 2 == 0).Sum());
+
+            //8の倍数の合計値
+            Console.WriteLine("8の倍数の合計値：" + numbers.Where(s => s % 8 == 0).Sum());
+
+
+            //foreach (var num in numbers) {
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
