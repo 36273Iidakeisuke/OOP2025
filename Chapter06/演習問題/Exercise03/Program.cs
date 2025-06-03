fade6a2d7d60874eaa0838ff0005f1d1aa2a4e19
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-06-03 10:33:02 +0900

    6-4完成

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+        }
+
+        private static void Exercise1(string text) {
+            var spaces = text.Count(c => c == ' ');
+            //var spaces = text.Count(char.IsWhiteSpace);//別解
+            Console.WriteLine("空白数:{0}", spaces);
+        }
+
+        private static void Exercise2(string text) {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            //やらなくてよい
+        }
+
+        private static void Exercise4(string text) {
+            var count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}", count);
+        }
+
+        private static void Exercise5(string text) {
+            var words = text.Split(' ').Where(s => s.Length <= 4);
+
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            var array = line.Split(';');
+
+            for (int i = 0; i < array.Length; i++) {
+                var words = array[i].Split('=');
+                Console.WriteLine(ToJapanese(words[0]) + "：" + words[1]);
+            }
+        }
+
+
+
+        /// <summary>
+        /// 引数の単語を日本語へ変換します
+        /// </summary>
+        /// <param name="key">"Novelist","BestWork","Born"</param>
+        /// <returns>"「作家」,「代表作」,「誕生年」</returns>
+        static string ToJapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+            }
+            return ""; //エラーをなくすためのダミー
+        }
+    }
+}
