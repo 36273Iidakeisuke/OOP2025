1fa663fd159b219c4db15fdd4fd2cce630ed3691
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-06-11 10:59:10 +0900

    8-1-1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+
+
+        }
+
+        private static void Exercise1(string text) {
+            var dictionary = new Dictionary<char, int>();
+            var work = text.ToUpper();
+            foreach (var c in work) {
+                if ('A' <= c && c <= 'Z') {
+                    if (dictionary.ContainsKey(c)) {
+                        dictionary[c] ++;
+                    } else {
+                        dictionary[c] = 1;
+                    }
+                }
+            }
+            
+            foreach (var item in dictionary.OrderBy(p => p.Key)) {
+                Console.WriteLine(item.Key + "：" +item.Value);
+            }
+
+        }
+
+        private static void Exercise2(string text) {
+
+        }
+    }
+}
