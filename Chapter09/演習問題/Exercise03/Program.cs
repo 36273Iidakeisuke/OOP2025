70cc6f62043274fbe791f95f4a055dca6551b210
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-06-27 13:47:59 +0900

    9-3-3手直し

--- Chapter09/演習問題/Exercise03/Program.cs
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -1,29 +1,32 @@
-﻿namespace Exercise03 {
+﻿using System.Diagnostics;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            var tw = new TimeWatch();
+            //var tw = new TimeWatch();
+            Stopwatch tw = new Stopwatch();
             tw.Start();
             // スリープする
             Thread.Sleep(1000);
-            TimeSpan duration = tw.Stop();
-            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
-
+            tw.Stop();
+            Console.WriteLine("処理時間は{0}ミリ秒でした", tw.Elapsed.TotalMilliseconds);
+            
         }
     }
 
-    class TimeWatch {
-        private DateTime _time;
+    //class TimeWatch {
+    //    private DateTime _time;
 
-        public void Start() {
-            //現在の時間を_timeに設定
-            _time = DateTime.Now;
+    //    public void Start() {
+    //        //現在の時間を_timeに設定
+    //        _time = DateTime.Now;
 
-        }
+    //    }
 
-        public TimeSpan Stop() {
-            //経過時間を返却する
-            return DateTime.Now - _time; // ←エラーを出さないためだけのダミー（使い方も参考にしない）
-        }
-    }
+    //    public TimeSpan Stop() {
+    //        //経過時間を返却する
+    //        return DateTime.Now - _time; // ←エラーを出さないためだけのダミー（使い方も参考にしない）
+    //    }
+    //}
 }
 
