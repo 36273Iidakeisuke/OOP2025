51882ba11ff9c8705cb2adfa0359c8a1ee687082
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-10-02 14:59:33 +0900

    ボタンによる文字列の変更

---
+++ WPF/新しいフォルダー/HelloWorld/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class ViewModel : BindableBase
+    {
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                () => GreetingMesseage = "Bye-bye world");
+        }
+        private string _greetingMessage = "Hello World!";
+        public string GreetingMesseage {
+            get=>_greetingMessage;
+            set => SetProperty(ref _greetingMessage, value);
+        }
+        public DelegateCommand ChangeMessageCommand { get; }
+    }
+}
