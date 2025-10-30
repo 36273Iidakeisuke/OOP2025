b4304bc4cc8901dcaa1d11b93df7478ed818df88
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-10-30 15:08:07 +0900

    コード14_8

--- Chapter14/Section03/Form1.cs
+++ Chapter14/Section03/Form1.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-
 namespace Section03 {
     public partial class Form1 : Form {
         public Form1() {
@@ -8,16 +6,15 @@ namespace Section03 {
 
         private async void button1_Click(object sender, EventArgs e) {
             toolStripStatusLabel1.Text = "";
-            var elapsed = await Task.Run(() => DoLongTimeWork());
+            await DoLongTimeWork();
             toolStripStatusLabel1.Text = "終了";
         }
 
-        //戻り値のある同期メソッド
-        private long DoLongTimeWork() {
-            var sw = Stopwatch.StartNew();
-            System.Threading.Thread.Sleep(5000);
-            sw.Stop();
-            return sw.ElapsedMilliseconds;
+        //非同期メソッド
+        private async Task DoLongTimeWork() {
+            await Task.Run(() => {
+                System.Threading.Thread.Sleep(5000);
+            });
         }
     }
 }
