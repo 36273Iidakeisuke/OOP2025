5cbb4c90861d5ea087b8cfff1cec87f23af072de
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-11-06 14:43:21 +0900

    WebBrowser完成

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,8 +20,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        WebView.EnsureCoreWebView2Async(null);
+        InitialzeAsync();
     }
+
+    private async void InitialzeAsync() {
+        await WebView.EnsureCoreWebView2Async();//非同期にしてブラウザの初期化処理をしている
+
+        WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+    }
+
+
+    //読み込みが開始したらプログレスバーを表示
+    private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LoadingBar.Visibility = Visibility.Visible;
+        LoadingBar.IsIndeterminate = true;
+    }
+
+    //読み込みが完了したらプログレスバーを非表示
+    private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+        LoadingBar.Visibility = Visibility.Collapsed;
+        LoadingBar.IsIndeterminate = false;
+    }
+
 
     private void BackButton_Click(object sender, RoutedEventArgs e) {
         // WebView2 に戻る履歴があるかチェック
@@ -37,6 +59,11 @@ public partial class MainWindow : Window
     }
 
     private void GoButton_Click(object sender, RoutedEventArgs e) {
-            WebView.Source = new Uri(AddressBar.Text);
+        //WebView.Source = new Uri(AddressBar.Text);
+        var url = AddressBar.Text.Trim();
+
+        if (string.IsNullOrWhiteSpace(url)) return;
+
+        WebView.Source = new Uri(url);
     }
 }
