12dfd137ad5f862949d942c62a64af88c94ad55c
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-11-10 14:17:41 +0900

    10,2完成

--- Chapter10/Exercise02/Program.cs
+++ Chapter10/Exercise02/Program.cs
@@ -2,12 +2,13 @@
     internal class Program {
         static void Main(string[] args) {
             var filePath = "source.txt";
+            var newFilePath = "newSource.txt";
             var line = File.ReadLines(filePath)
                 .Select((s, ix) => $"{ix + 1,4}: {s}");
             foreach (var item in line) {
                 Console.WriteLine(item);
             }
-
+            File.WriteAllLines(newFilePath, line);
 
         }
     }
