2230bf08928f1b448a4e30d47814a3bec99caec7
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-11-10 14:01:43 +0900

    10.10.1完成

---
+++ Chapter10/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = "source.txt";
+            if (File.Exists(filePath)) {
+                using var reader = new StreamReader(filePath);
+                int cnt = 0;
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine() ?? string.Empty;
+                    //if (Regex.IsMatch(line, @"\sclass\s")) {
+                    //    cnt++;
+                    //}
+                    string[] words = line.Split(' ');
+                    var word = words.Where(s => s.Contains("class"));
+                    if (word.Count() > 0) {
+                        cnt++;
+                    }
+                }
+
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,39 @@
+﻿using Section01;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Section05 {
+    internal class Program {
+        static async Task Main(string[] args) {
+
+            var ta = new TaskExample();
+            await ta.Run();
+        }
+    }
+
+
+
+    // Code 14.22
+    public class TaskExample {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task Run() {
+            var tasks = new Task<string>[] {
+            GetPageAsync(@"https://business.nikkei.com/rss/sns/nb.rdf"),
+            GetPageAsync(@"https://www.nhk.or.jp/rss/news/cat3.xml"),
+        };
+            var results = await Task.WhenAll(tasks);
+
+            // それぞれ先頭400文字を表示する
+            var text = $"{results[0].Substring(0, 400)}\n\n{results[1].Substring(0, 400)}";
+            Console.WriteLine(text);
+        }
+
+
+        private async Task<string> GetPageAsync(string urlstr) {
+            var str = await _httpClient.GetStringAsync(urlstr);
+            return str;
+        }
+
+    }
+}
