e0c083d952bf91da54d1a1d22f6c39ac8c6efb5d
Iidakeisuke <oibc36273@ojs.ac.jp>
2025-11-12 10:13:02 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter10/Exercise03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise03 {
+    internal class Program {
+        static
+            void
+            Main
+            (string[] args) 
+            {
+            var filePath = "source1.txt";
+            var filePath2 = "source2.txt";
+            var newFilePath = "newSource.txt";
+            var line = File.ReadLines(filePath);
+            var line2 = File.ReadLines(filePath2);
+            var line3 = new List<string>();
+            line3.AddRange(line);
+            line3.AddRange(line2);
+
+            File.WriteAllLines(newFilePath,line3);
+        }
+    }
+}
---
+++ Chapter10/Exercise04/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var files = getAllFiles("C:\\Users\\infosys\\source\\repos\\OOP2025");
+
+            var fFiles = files.Select(s=>new FileInfo(s)).Where(fi => fi.Length >= 1048576);
+
+            foreach (var file in fFiles) {
+                Console.WriteLine($"{file.Length/1048576, 5} MB : {Path.GetFileName(file.Name)}");
+            }
+        }
+
+
+        public static List<string> getAllFiles(string path) {
+            var work = new List<string>();
+            if (Directory.Exists(path)) {
+                var files = Directory.GetFiles(path);
+                var dirs = Directory.GetDirectories(path);
+                work.AddRange(files);
+
+                foreach (var file in dirs) {
+                    var t = getAllFiles(file);
+                    work.AddRange(t);
+                }
+            } else if (File.Exists(path)) {
+                work.Add(path);
+            }
+            return work;
+
+        }
+    }
+}
